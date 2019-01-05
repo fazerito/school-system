d0122eadff9a9d2f195436c0a572700de55651e9
fazerito <gervvaz@gmail.com>
2019-01-05 19:00:41 +0100

    added  class controller + views

---
+++ Controllers/ClassController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SchoolProject.Models;
+
+namespace SchoolProject.Controllers
+{
+    public class ClassController : Controller
+    {
+        private readonly SchoolDbContext _context;
+
+        public ClassController(SchoolDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Class
+        [Route("classes/get")]
+        public async Task<IActionResult> Index()
+        {
+            var classes = _context.Classes.Include(c => c.Teacher.PersonalData);
+
+            
+            return View(await classes.ToListAsync());
+        }
+
+        // GET: Class/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var classes = await _context.Classes
+                .Include(c => c.Teacher.PersonalData)
+                .FirstOrDefaultAsync(m => m.ClassId == id);
+
+            var studentsInClass = _context.Classes
+                .Where(c => c.ClassId == id)
+                .SelectMany(s => s.Students)
+                .Where(s => s.ClassId == id)
+                .Include(s => s.PersonalData)
+                .ToList();
+
+            
+
+            if (classes == null)
+            {
+                return NotFound();
+            }
+
+            classes.Students = studentsInClass;
+
+            return View(classes);
+        }
+
+        // GET: Class/Create
+        public IActionResult Create()
+        {
+            ViewData["TeacherId"] = new SelectList(_context.Teachers, "TeacherId", "TeacherId");
+            return View();
+        }
+
+        // POST: Class/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ClassId,Name,TeacherId")] Classes classes)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(classes);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TeacherId"] = new SelectList(_context.Teachers, "TeacherId", "TeacherId", classes.TeacherId);
+            return View(classes);
+        }
+
+        // GET: Class/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var classes = await _context.Classes.FindAsync(id);
+            if (classes == null)
+            {
+                return NotFound();
+            }
+            ViewData["TeacherId"] = new SelectList(_context.Teachers, "TeacherId", "TeacherId", classes.TeacherId);
+            return View(classes);
+        }
+
+        // POST: Class/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ClassId,Name,TeacherId")] Classes classes)
+        {
+            if (id != classes.ClassId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(classes);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClassesExists(classes.ClassId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TeacherId"] = new SelectList(_context.Teachers, "TeacherId", "TeacherId", classes.TeacherId);
+            return View(classes);
+        }
+
+        // GET: Class/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var classes = await _context.Classes
+                .Include(c => c.Teacher)
+                .FirstOrDefaultAsync(m => m.ClassId == id);
+            if (classes == null)
+            {
+                return NotFound();
+            }
+
+            return View(classes);
+        }
+
+        // POST: Class/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var classes = await _context.Classes.FindAsync(id);
+            _context.Classes.Remove(classes);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClassesExists(int id)
+        {
+            return _context.Classes.Any(e => e.ClassId == id);
+        }
+    }
+}
